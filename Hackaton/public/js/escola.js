9c367154ada8d5a173d54cd0f3fdf3f8d8a7f8d5
LinaAmaral <lina.amaralcaetano@gmail.com>
2021-09-11 14:31:18 -0300

    Página escola

---
+++ Hackaton/public/js/escola.js
@@ -0,0 +1,32 @@
+$("#posicaoRanking").hide();
+$("#extratoBem").hide();
+$("#classificaoAlunos").hide();
+$("#msgMetal").hide();
+$("#msgPlastico").hide();
+$("#msgVidro").hide();
+$("#msgTnt").hide();
+
+$(document).ready(function () {
+
+    $("#AtivaPosicaoRanking").click(function(){
+        $("#posicaoRanking").toggle();
+    })
+    $("#AtivaextratoBem").click(function(){
+        $("#extratoBem").toggle();
+    })
+    $("#AtivaclassificaoAlunos").click(function(){
+        $("#classificaoAlunos").toggle();
+    })
+    $("#saco_vidro").click(function(){
+        $("#msgVidro").show();
+    })
+    $("#saco_metal").click(function(){
+        $("#msgMetal").show();
+    })
+    $("#saco_plastico").click(function(){
+        $("#msgPlastico").show();
+    })
+    $("#saco_tnt").click(function(){
+        $("#msgTnt").show();
+    })
+})
